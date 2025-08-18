28f90830a821d350505dc15f6da31dc6ba8978ec
epistemik <epistemik@gmail.com>
2025-08-18 13:04:57 -0400

    file name changes and minor updates

---
+++ spellingbeeGameEngine.py
@@ -0,0 +1,20 @@
+##############################################################################################################################
+# coding=utf-8
+#
+# spellingbeeGameEngine.py
+#   -- SpellingBee game engine
+#
+# Copyright (c) 2025 Mark Sattolo <epistemik@gmail.com>
+
+__author_name__    = "Mark Sattolo"
+__author_email__   = "epistemik@gmail.com"
+__python_version__ = "3.10+"
+__created__ = "2025-08-18"
+__updated__ = "2025-08-18"
+
+from sys import path, argv
+from abc import ABC, abstractmethod
+from argparse import ArgumentParser
+path.append("/home/marksa/git/Python/utils")
+from mhsUtils import *
+from mhsLogging import *
--- spellingbeePyside6_UI.py
+++ spellingbeePyside6_UI.py
@@ -1,28 +1,21 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# pyside6-UI.py
-#   -- 
+# spellingbee-pyside6-UI.py
+#   -- launch the UI to play the SpellingBee game
 #
 # Copyright (c) 2025 Mark Sattolo <epistemik@gmail.com>
 
 __author_name__    = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.10+"
-__created__ = "2024-07-01"
-__updated__ = "2025-08-01"
+__created__ = "2025-08-18"
+__updated__ = "2025-08-18"
 
-from sys import path
 from PySide6.QtWidgets import (QApplication, QComboBox, QVBoxLayout, QGroupBox, QDialog, QFileDialog, QLabel, QCheckBox,
                                QPushButton, QFormLayout, QDialogButtonBox, QTextEdit, QInputDialog, QMessageBox)
 from functools import partial
-path.append("/home/marksa/git/Python/utils")
-from sys import path, argv
-from abc import ABC, abstractmethod
-from argparse import ArgumentParser
-path.append("/home/marksa/git/Python/utils")
-from mhsUtils import *
-from mhsLogging import *
+from spellingbeeGameEngine import *
 
 UI_DEFAULT_LOG_LEVEL:int = logging.INFO
 
