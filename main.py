c3897ffbc79ca51906f2dbdd41bdbe21b90e155d
epistemik <epistemik@gmail.com>
2025-11-09 17:56:29 -0500

    game launch script

---
+++ main.py
@@ -0,0 +1,41 @@
+##############################################################################################################################
+# coding=utf-8
+#
+# main.py
+#   -- launch the SpellingBee game
+#
+# Copyright (c) 2025 Mark Sattolo <epistemik@gmail.com>
+
+__author_name__    = "Mark Sattolo"
+__author_email__   = "epistemik@gmail.com"
+__python_version__ = "3.10+"
+__created__ = "2025-11-08"
+__updated__ = "2025-11-08"
+
+from spellingbeePyside6_UI import *
+
+if __name__ == "__main__":
+    log_control = MhsLogger(SpellingBeeUI.__name__, con_level = DEFAULT_LOG_LEVEL)
+    dialog = None
+    app = None
+    code = 0
+    try:
+        app = QApplication(argv)
+        dialog = SpellingBeeUI()
+        dialog.show()
+        app.exec()
+    except KeyboardInterrupt as mki:
+        log_control.exception(mki)
+        code = 13
+    except ValueError as mve:
+        log_control.error(mve)
+        code = 27
+    except Exception as mex:
+        log_control.exception(mex)
+        code = 66
+    finally:
+        if dialog:
+            dialog.close()
+        if app:
+            app.exit(code)
+    exit(code)
