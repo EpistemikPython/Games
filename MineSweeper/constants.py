027832daa79c78a4bde5a57d9e8e6ca4aa58befc
epistemik <epistemik@gmail.com>
2025-11-16 13:30:09 -0500

    change selection of colors and images; set out limits for grid dimensions and number of mines

--- MineSweeper/constants.py
+++ MineSweeper/constants.py
@@ -4,7 +4,8 @@
 # constants.py
 #   -- MineSweeper game constants
 #
-# >> based on code by Martin Fitzpatrick @ https://github.com/pythonguis/pythonguis-examples
+# >> based on code by Martin Fitzpatrick
+#    @ https://github.com/pythonguis/pythonguis-examples/tree/master/pyside6/demos/minesweeper
 #
 # Copyright (c) 2025 Mark Sattolo <epistemik@gmail.com>
 
@@ -12,29 +13,38 @@ __author_name__    = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.10+"
 __created__ = "2025-11-12"
-__updated__ = "2025-11-13"
+__updated__ = "2025-11-16"
 
 from enum import IntEnum
-from PySide6.QtGui import (QColor, QImage)
+from PySide6.QtGui import QImage, QColor, QColorConstants
 
-IMG_BOMB = QImage("./images/bug.png")
-IMG_FLAG = QImage("./images/flag.png")
-IMG_START = QImage("./images/rocket.png")
-IMG_CLOCK = QImage("./images/clock-select.png")
+IMG_BOMB         = QImage("images/bug.png")
+IMG_FLAG         = QImage("images/flag.png")
+IMG_BAD_FLAG     = QImage("images/mushroom.png")
+IMG_MISSING_FLAG = QImage("images/hamburger.png")
+IMG_START        = QImage("images/ice-cream-sprinkles.png")
+IMG_PLAY         = QImage("images/fruit.png")
+IMG_WIN          = QImage("images/cake.png")
+IMG_LOSE         = QImage("images/cactus.png")
 
-NUM_COLORS = {
-    1: QColor("#f44336"),
-    2: QColor("#9C27B0"),
-    3: QColor("#3F51B5"),
-    4: QColor("#03A9F4"),
-    5: QColor("#00BCD4"),
-    6: QColor("#4CAF50"),
-    7: QColor("#E91E63"),
-    8: QColor("#FF9800"),
+SQUARE_COLORS = {
+    1: QColor(QColorConstants.Red),
+    2: QColor(QColorConstants.Blue),
+    3: QColor(QColorConstants.DarkGreen),
+    4: QColor(QColorConstants.Magenta),
+    5: QColor(QColorConstants.Cyan),
+    6: QColor(QColorConstants.DarkYellow),
+    7: QColor(QColorConstants.Gray),
+    8: QColor(QColorConstants.Black),
 }
 
-LEVELS = [(8, 10), (16, 40), (24, 99)]
+# length of grid in number of squares
+MIN_GRID_LEN = 8
+DEFAULT_GRID_LEN = 16
+LARGE_GRID_LEN = 24
+MAX_GRID_LEN = 32
 
+DEFAULT_NUM_MINES = 42
 
 class Status(IntEnum):
     READY = 0
@@ -44,8 +54,8 @@ class Status(IntEnum):
 
 
 STATUS_ICONS = {
-    Status.READY: "./images/plus.png",
+    Status.READY:   "./images/plus.png",
     Status.PLAYING: "./images/smiley.png",
-    Status.FAILED: "./images/cross.png",
+    Status.FAILED:  "./images/cross.png",
     Status.SUCCESS: "./images/smiley-lol.png",
 }
