6b5ee00705db1252272c2baf598e8105ce6a83a3
epistemik <epistemik@gmail.com>
2026-07-12 11:06:41 -0400

    handle invalid arguments

--- Wordle/main.py
+++ Wordle/main.py
@@ -10,13 +10,14 @@ __author_name__    = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.11+"
 __created__ = "2026-03-05"
-__updated__ = "2026-07-11"
+__updated__ = "2026-07-12"
 
 from wordleGame import *
 
 if __name__ == "__main__":
+    usage_text = f"Usage: python3 {get_filename(argv[0])} [$word_length] [$num_rows]\n"
     if len(argv) > 3:
-        print(f"Usage: python3 {get_filename(argv[0])}\nLaunch the Wordle game.")
+        print(usage_text)
         log_control.debug("Usage instructions.")
         exit(0)
     window = None
@@ -25,11 +26,19 @@ if __name__ == "__main__":
     try:
         app = QApplication(argv)
         if len(argv) > 2:
+            if not argv[1].isdigit() or not argv[2].isdigit():
+                print(usage_text)
+                log_control.debug("Invalid arguments.")
+                raise Exception("Invalid arguments.")
             window = WordleUI(int(argv[1]), int(argv[2]))
             print(f"argv[1]: {argv[1]}, argv[2]: {argv[2]}")
         elif len(argv) > 1:
-            window = WordleUI(int(argv[1]), 6)
-            print(f"argv[1]: {argv[1]}, p_rows = 6")
+            if not argv[1].isdigit():
+                print(usage_text)
+                log_control.debug("Invalid arguments.")
+                raise Exception("Invalid arguments.")
+            window = WordleUI(int(argv[1]))
+            print(f"argv[1]: {argv[1]}, p_rows = {DEFAULT_NUM_ROWS}")
         else:
             window = WordleUI()
             print("No arguments.")
@@ -38,7 +47,7 @@ if __name__ == "__main__":
         log_control.exception(mki)
         code = 13
     except ValueError as mve:
-        log_control.error(mve)
+        log_control.exception(mve)
         code = 27
     except Exception as mex:
         log_control.exception(mex)
